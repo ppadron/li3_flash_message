5a9ac61ca379b935eab923ece991f073ee718e73
Gavin Davies <gavindavies@gmail.com>
2016-03-03 11:29:39 +0000

    Updates to li3 1.1 filter format

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,10 +1,9 @@
 <?php
 
 use li3_flash_message\extensions\storage\FlashMessage;
-use lithium\action\Dispatcher;
 
-Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
-	$object = $chain->next($self, $params, $chain);
+\lithium\aop\Filters::apply('\lithium\action\Dispatcher', '_callable', function($params, $next) {
+	$object = $next($params);
 
 	if (is_a($object, 'lithium\action\Controller')) {
 		return FlashMessage::bindTo($object);
