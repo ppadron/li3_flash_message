60324ab5c45106b2ae49e9064473b77b9da9ec12
Pedro Padron <pedro@templateria.com>
2018-11-08 18:57:17 -0200

    Another applyFilter updated.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,8 +3,8 @@
 use li3_flash_message\extensions\storage\FlashMessage;
 use lithium\action\Dispatcher;
 
-Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
-	$object = $chain->next($self, $params, $chain);
+Dispatcher::applyFilter('_callable', function($params, $next) {
+	$object = $next($params);
 
 	if (is_a($object, 'lithium\action\Controller')) {
 		return FlashMessage::bindTo($object);
